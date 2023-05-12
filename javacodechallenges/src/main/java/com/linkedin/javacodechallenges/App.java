1adc4f0a66fc3f0c042cb74fafb10b503efd734a
katarzynach <kasia97h@o2.pl>
2023-05-12 12:38:55 +0200

    02_01_solution - date in 100 days

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,18 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class App {
     // Create function to calculate the date that's 
     // 100 days from now
 
-    public static void main( String[] args )
-    {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+    public static void main(String[] args) {
+        System.out.println("100 days from now is... " + getDate100DaysFromToday());
+    }
+
+    public static LocalDate getDate100DaysFromToday() {
+        LocalDate today = LocalDate.now(ZoneId.of("Europe/Paris"));
+        return today.plusDays(100);
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,20 +1,18 @@
 package com.linkedin.javacodechallenges;
 
+import static com.linkedin.javacodechallenges.App.getDate100DaysFromToday;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.time.LocalDate;
+import java.util.Locale;
+
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void checkDateIn100Days() {
+        LocalDate dateIn100Days = getDate100DaysFromToday();
+        assertEquals(LocalDate.of(2023, 8, 20), dateIn100Days);
     }
 }
